16dd88ee6014e8c7d6ce08386319fc4299abf419
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-10-27 12:04:54 -0300

    Send dynamic END

--- middleware/__main__.py
+++ middleware/__main__.py
@@ -20,17 +20,23 @@ logger.debug("Waiting for reducer spawner signal")
 reducer_spawner_client.recv()
 logger.debug("Signal received, sending data to reducers")
 
+keys = {}
 for _ in range(10):
     logger.debug("Receiving message from mappers")
-    message = server.recv_multipart()  # FIXME why
+    message = server.recv_multipart()  # FIXME why do I have to recv a multipart if mapper sent me a string
     logger.debug("Multipart received %r", message)
-    key, value = message[3].split('#'.encode())  # FIXME WHYYYY
+    key, value = message[3].split('#'.encode())
+
+    if key not in keys:
+        keys[key] = True
+
     logger.debug("Message received %r, key %r, value %r", message[3], key, value)
     logger.debug("Sending message to reducer")
     server.send_multipart([key, value])
     logger.debug("Task sent")
 
-server.send_multipart([b'A', b'END'])  # FIXME implement for dynamic keys
-server.send_multipart([b'B', b'END'])
+logger.debug("Sending END to reducers")
+for key in keys:
+    server.send_multipart([key, b'END'])
 logger.debug("End")
 
