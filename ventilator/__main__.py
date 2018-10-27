b5dff13ca762b1acf8690d38d288563d583ad38e
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-10-27 20:29:43 -0300

    Ventilator refactor

--- ventilator/__main__.py
+++ ventilator/__main__.py
@@ -1,15 +1,9 @@
 import os
 import logging
 
-import zmq
-
-from .dataset_handler import DatasetHandler
+from .data_ventilator import DataVentilator
 
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)s:%(name)s:%(threadName)s: %(message)s")
-logger = logging.getLogger("Ventilator")
-
-# Takes N and dir of dataset
-# Partitions all data in N and sends one partition to each mapper
 
 N = int(os.environ['MAPPERS'])  # Mappers quantity
 DATASET_DIR = os.environ['DATASET_DIR']
@@ -17,35 +11,14 @@ DATASET_DIR = os.environ['DATASET_DIR']
 ventilator_endpoint = "tcp://0.0.0.0:5562"
 mappers_ready_endpoint = "tcp://0.0.0.0:5563"
 
-logger.debug("Start")
-context = zmq.Context()
 
-sender = context.socket(zmq.PUSH)
-sender.bind(ventilator_endpoint)
-
-mappers_ready_ack = context.socket(zmq.PULL)
-mappers_ready_ack.bind(mappers_ready_endpoint)
-
-logger.debug("Waiting for mappers ready ACK")
-for _ in range(N):
-    ack = mappers_ready_ack.recv()
-    logger.debug("Mapper ACK received: %r", ack)
+def main():
+    logger = logging.getLogger("Ventilator")
+    logger.debug("Start")
+    ventilator = DataVentilator(N, ventilator_endpoint, mappers_ready_endpoint)
+    ventilator.start(DATASET_DIR)
+    logger.debug("End")
 
 
-logger.debug("Sending data to mappers")
-set_handler = DatasetHandler(DATASET_DIR)
-
-shotlogs = set_handler.get_shotlogs()
-for log in shotlogs:
-    logger.debug("Reading shotlog: %s", log)
-    with open(log, "r") as file:
-        line = file.readline()
-        while line:
-            logger.debug("Read line: %s", line)
-            sender.send_string(line)
-            line = file.readline()
-
-for _ in range(N):
-    sender.send_string("END")
-
-logger.debug("End")
+if __name__ == "__main__":
+    main()
---
+++ ventilator/data_ventilator.py
@@ -0,0 +1,58 @@
+import logging
+
+import zmq
+
+from .dataset_handler import DatasetHandler
+
+
+class DataVentilator:
+
+    class MappersConnection:
+        def __init__(self, mappers, ventilator_endpoint, mappers_ready_endpoint):
+            self.mappers = mappers
+            self.logger = logging.getLogger("MapperConnection")
+
+            context = zmq.Context()
+
+            self.sender = context.socket(zmq.PUSH)
+            self.sender.bind(ventilator_endpoint)
+
+            mappers_ready_ack = context.socket(zmq.PULL)
+            mappers_ready_ack.bind(mappers_ready_endpoint)
+
+            self.logger.debug("Waiting for mappers ready ACK")
+            for _ in range(self.mappers):
+                ack = mappers_ready_ack.recv()
+                self.logger.debug("Mapper ACK received: %r", ack)
+
+        def send(self, data):
+            self.sender.send_string(data)
+
+        def close(self):
+            for _ in range(self.mappers):
+                self.sender.send_string("END")
+
+    def __init__(self, mappers, ventilator_endpoint, mappers_ready_endpoint):
+        self.logger = logging.getLogger("DataVentilator")
+        self.mappers = mappers
+        self.ventilator_endpoint = ventilator_endpoint
+        self.mappers_ready_endpoint = mappers_ready_endpoint
+        self.mappers_conn = None
+
+    def start(self, dataset_dir):
+        self.mappers_conn = self.MappersConnection(self.mappers, self.ventilator_endpoint, self.mappers_ready_endpoint)
+
+        self.logger.debug("Sending data to mappers")
+        set_handler = DatasetHandler(dataset_dir)
+
+        shotlogs = set_handler.get_shotlogs()
+        for log in shotlogs:
+            self.logger.debug("Reading shotlog: %s", log)
+            with open(log, "r") as file:
+                line = file.readline()
+                while line:
+                    self.logger.debug("Read line: %s", line)
+                    self.mappers_conn.send(line)
+                    line = file.readline()
+
+        self.mappers_conn.close()
