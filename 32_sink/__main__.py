d441a8a11eb0b20ab1ed8f3ba09a678d6d075baa
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-11-06 16:52:48 -0300

    Multiply by 100

--- 32_sink/__main__.py
+++ 32_sink/__main__.py
@@ -13,12 +13,12 @@ collector_endpoint = os.environ['COLLECTOR_ENDPOINT']
 
 def format_result(result):
     if result[1][2][1] > 0:
-        tuple_2 = (result[0], 2, result[1][2][0]/result[1][2][1])
+        tuple_2 = (result[0], 2, result[1][2][0]*100/result[1][2][1])
     else:
         tuple_2 = (result[0], 2, 0)
 
     if result[1][3][1] > 0:
-        tuple_3 = (result[0], 3, result[1][3][0]/result[1][3][1])
+        tuple_3 = (result[0], 3, result[1][3][0]*100/result[1][3][1])
     else:
         tuple_3 = (result[0], 3, 0)
 
