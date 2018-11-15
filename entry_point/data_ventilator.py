edc9c2673ed7559975d4d650804f9dc5fc1a9f32
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-11-15 11:13:27 -0300

    DataVentilator mw

--- entry_point/data_ventilator.py
+++ entry_point/data_ventilator.py
@@ -1,63 +1,21 @@
 import logging
 
-import zmq
-
 from .dataset_handler import DatasetHandler
+from middleware.entry import DataVentilatorMiddleware
 
 
 class DataVentilator:
 
-    class VentilatorsConnection:
-        def __init__(self, mapper_endpoint_1, mapper_endpoint_2, mapper_endpoint_3, dispatcher_ready_endpoint):
-            self.logger = logging.getLogger("VentilatorsConnection")
-
-            context = zmq.Context()
-
-            self.ack_server = context.socket(zmq.PULL)
-            self.ack_server.bind(dispatcher_ready_endpoint)
-
-            self.server_1 = context.socket(zmq.PUSH)
-            self.server_1.bind(mapper_endpoint_1)
-
-            self.server_2 = context.socket(zmq.PUSH)
-            self.server_2.bind(mapper_endpoint_2)
-
-            self.server_3 = context.socket(zmq.PUSH)
-            self.server_3.bind(mapper_endpoint_3)
-
-        def wait_for_ventilators(self):
-            for _ in range(3):
-                self.logger.debug("Waiting for shotlog_dispatcher ACK")
-                self.ack_server.recv()
-            self.logger.debug("Overall shotlog_dispatcher ACK received")
-
-        def send_shotlog(self, shotlog):
-            self.server_1.send_string(shotlog)
-            self.server_2.send_string(shotlog)
-            self.server_3.send_string(shotlog)
-
-        def close(self):
-            self.server_1.send_string("END")
-            self.server_2.send_string("END")
-            self.server_3.send_string("END")
-
     def __init__(self, ventilator_endpoint_1, ventilator_endpoint_2, ventilator_endpoint_3, dispatcher_ready_endpoint, dataset_dir):
-        self.logger = logging.getLogger("Trigger")
-        self.ventilator_endpoint_1 = ventilator_endpoint_1
-        self.ventilator_endpoint_2 = ventilator_endpoint_2
-        self.ventilator_endpoint_3 = ventilator_endpoint_3
-        self.dispatcher_ready_endpoint = dispatcher_ready_endpoint
+        self.logger = logging.getLogger("DataVentilator")
         self.dataset_dir = dataset_dir
+        self.conn = DataVentilatorMiddleware(ventilator_endpoint_1, ventilator_endpoint_2, ventilator_endpoint_3, dispatcher_ready_endpoint)
 
     def start(self):
-        ventilators_conn = self.VentilatorsConnection(self.ventilator_endpoint_1,
-                                                      self.ventilator_endpoint_2,
-                                                      self.ventilator_endpoint_3,
-                                                      self.dispatcher_ready_endpoint)
-        self.logger.debug("Waiting for ventilators")
-        ventilators_conn.wait_for_ventilators()
+        self.logger.debug("Waiting for dispatchers")
+        self.conn.wait_for_dispatchers()
 
-        self.logger.debug("Sending data to mappers")
+        self.logger.debug("Sending data to dispatchers")
         set_handler = DatasetHandler(self.dataset_dir)
 
         shotlogs = set_handler.get_shotlogs()
@@ -68,7 +26,7 @@ class DataVentilator:
                 line = file.readline()
                 while line:
                     self.logger.debug("Read line: %s", line)
-                    ventilators_conn.send_shotlog(line)
+                    self.conn.send_shotlog(line)
                     line = file.readline()
 
-        ventilators_conn.close()
+        self.conn.close()
--- middleware/entry.py
+++ middleware/entry.py
@@ -1,3 +1,5 @@
+import logging
+
 import zmq
 
 
@@ -17,3 +19,52 @@ class ResultsCollectorMiddleware:
 
     def receive_result(self):
         return self.sinks_conn.receive_result()
+
+
+class DataVentilatorMiddleware:
+
+    class ShotlogDisparchersConnection:
+        def __init__(self, ventilator_endpoint_1, ventilator_endpoint_2, ventilator_endpoint_3, dispatcher_ready_endpoint):
+            self.logger = logging.getLogger("VentilatorsConnection")
+
+            context = zmq.Context()
+
+            self.ack_server = context.socket(zmq.PULL)
+            self.ack_server.bind(dispatcher_ready_endpoint)
+
+            self.server_1 = context.socket(zmq.PUSH)
+            self.server_1.bind(ventilator_endpoint_1)
+
+            self.server_2 = context.socket(zmq.PUSH)
+            self.server_2.bind(ventilator_endpoint_2)
+
+            self.server_3 = context.socket(zmq.PUSH)
+            self.server_3.bind(ventilator_endpoint_3)
+
+        def wait_for_dispatchers(self):
+            for _ in range(3):
+                self.logger.debug("Waiting for shotlog_dispatcher ACK")
+                self.ack_server.recv()
+            self.logger.debug("Overall shotlog_dispatcher ACK received")
+
+        def send_shotlog(self, shotlog):
+            self.server_1.send_string(shotlog)
+            self.server_2.send_string(shotlog)
+            self.server_3.send_string(shotlog)
+
+        def close(self):
+            self.server_1.send_string("END")
+            self.server_2.send_string("END")
+            self.server_3.send_string("END")
+
+    def __init__(self, ventilator_endpoint_1, ventilator_endpoint_2, ventilator_endpoint_3, dispatcher_ready_endpoint):
+        self.dispatchers_conn = self.ShotlogDisparchersConnection(ventilator_endpoint_1, ventilator_endpoint_2, ventilator_endpoint_3, dispatcher_ready_endpoint)
+
+    def wait_for_dispatchers(self):
+        self.dispatchers_conn.wait_for_dispatchers()
+
+    def send_shotlog(self, line):
+        self.dispatchers_conn.send_shotlog(line)
+
+    def close(self):
+        self.dispatchers_conn.close()
