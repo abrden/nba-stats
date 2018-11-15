37e1ea7588a3300c64255108f5c80aa750131c0b
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-11-15 09:33:18 -0300

    ReducerHandler mw

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 ```
 $ sudo docker-compose build
 
-$ sudo docker-compose up --scale 1_mapper=2 --scale 2_mapper=2
+$ sudo docker-compose up --scale 1_mapper=2 --scale 2_mapper=2 --scale 31_mapper=2 --scale 32_mapper=2 --scale 1_reducer=2 --scale 2_reducer=2 --scale 31_reducer=2 --scale 32_reducer=2 --force-recreate
 ```
--- mapper/mapper.py
+++ mapper/mapper.py
@@ -7,15 +7,9 @@ class Mapper:
 
     def __init__(self, mw_endpoint, key_queue_endpoint, ventilator_endpoint, mappers_ready_endpoint):
         self.logger = logging.getLogger("Mapper")
-        self.mw_endpoint = mw_endpoint
-        self.key_queue_endpoint = key_queue_endpoint
-        self.ventilator_endpoint = ventilator_endpoint
-        self.mappers_ready_endpoint = mappers_ready_endpoint
-        self.conn = None
+        self.conn = MapperMiddleware(mw_endpoint, key_queue_endpoint, ventilator_endpoint, mappers_ready_endpoint)
 
     def start(self, fun):
-        self.conn = MapperMiddleware(self.mw_endpoint, self.key_queue_endpoint, self.ventilator_endpoint, self.mappers_ready_endpoint)
-
         while True:
             self.logger.debug("Waiting for task from shotlog_dispatcher")
             task = self.conn.receive_task()
